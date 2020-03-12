4959594908f9c50e2092fba5044c12a456f9162a
Xiang <xn324197@dal.ca>
2020-03-12 20:13:55 -0300

    54. sprial matrix 55. Jump game

---
+++ Top150/55.JumpGame.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  if (nums.length === 1)  return true;
+  let max = 0;
+  for (let i = 0; i <nums.length; i ++ ) {
+    if (i > max ) return false;
+    max = Math.max(max, i + nums[i]);
+  }
+  return true;
+};
