a0071acb9cfa4e14f71beba40bb9254082779911
Xiang <xn324197@dal.ca>
2020-04-07 19:21:35 -0300

    162

---
+++ Top150/162.FindPeakElement.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+  if (nums.length === 1) return 0;
+  let left = 0,
+    right = nums.length - 1;
+  while (left < right) {
+    const mid = (left + (right - left) / 2) | 0;
+    if (nums[mid] < nums[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+};
