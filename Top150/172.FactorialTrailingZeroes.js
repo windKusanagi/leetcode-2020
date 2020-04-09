fd355780ba8458bb46802317d1695524500fc575
Xiang <xn324197@dal.ca>
2020-04-09 19:09:33 -0300

    172

---
+++ Top150/172.FactorialTrailingZeroes.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+  let res = 0;
+
+  while (n >= 5) {
+    res += (n / 5) | 0;
+    n = (n / 5) | 0;
+  }
+  return res;
+};
